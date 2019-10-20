65142df57e0e8d5cf172ea89e3d5123afe885402
Dodo(NickName) <rabbit.white@daum.net>
2019-10-20 20:53:02 +0900

    Signed-off-by: Dodo(NickName) <rabbit.white@daum.net>

---
+++ JasperMultiThreadServer/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace JasperMultiThreadServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOnOff = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOnOff
+            // 
+            this.btnOnOff.Location = new System.Drawing.Point(15, 313);
+            this.btnOnOff.Name = "btnOnOff";
+            this.btnOnOff.Size = new System.Drawing.Size(773, 33);
+            this.btnOnOff.TabIndex = 0;
+            this.btnOnOff.Text = "시작(Start)";
+            this.btnOnOff.UseVisualStyleBackColor = true;
+            this.btnOnOff.Click += new System.EventHandler(this.btnOnOff_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(15, 123);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(773, 184);
+            this.txtLog.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "서버(Server)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 358);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnOnOff);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOnOff;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JasperMultiThreadServer/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JasperMultiThreadServer
+{
+    public partial class Form1 : Form
+    {
+        int state;
+        Thread thConnect;
+        TcpClient thClientSocket;
+        TcpListener serverSocket;
+        TcpClient clientSocket;
+        string clNo;
+        int port;
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        public void init()
+        {
+            this.port = 8888;
+            state = 0;
+            txtLog.Text = DateTime.Now.ToString() + " 환영합니다." ;
+            serverSocket = null;
+            clientSocket = null;
+        }
+
+        [Obsolete]
+        private void btnOnOff_Click(object sender, EventArgs e)
+        {
+            if (state == 0)
+            {
+                thConnect = new Thread(serviceOn);
+                thConnect.Start();
+                state = 1;
+                btnOnOff.Text = "종료(Stop)";
+            }
+            else
+            {
+                if( serverSocket != null )
+                    serverSocket = null;
+
+                if( clientSocket != null )
+                    clientSocket.Close();
+
+                if ( thClientSocket != null )
+                    thClientSocket.Close();
+
+
+                MessageBox.Show("서버를 종료합니다.(Shutdown the server.)", "메시지(Messages)",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.ExitThread();
+                Environment.Exit(0);
+
+                thConnect.Abort();
+                state = 0;
+                btnOnOff.Text = "시작(Start)";
+            }
+            
+        }
+
+        [Obsolete]
+        public void serviceOn()
+        {
+            serverSocket = new TcpListener(port);
+            clientSocket = default(TcpClient);
+            int counter;
+
+            try
+            {
+                serverSocket.Start();   // 서버 시작
+            }
+            catch(Exception ex)
+            {
+                this.Invoke(new EventHandler(delegate {
+                    txtLog.Text = txtLog.Text + Environment.NewLine +
+                                  ex.ToString();
+                }));
+            }
+
+            counter = 0;
+
+            this.Invoke(new EventHandler(delegate {
+                txtLog.Text = txtLog.Text + Environment.NewLine +
+                              ">> 서버 시작되었음.(Server Started)";
+            }));
+
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                this.Invoke(new EventHandler(delegate {
+                    txtLog.Text = txtLog.Text +
+                              Environment.NewLine +
+                              ">> 클라이언트 번호(Client No):" +
+                              Convert.ToString(counter);
+                }));
+              
+
+                startClient(clientSocket, Convert.ToString(counter));
+
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+
+            this.Invoke(new EventHandler(delegate {
+                txtLog.Text = txtLog.Text + Environment.NewLine + ">> 서버 탈출(Server Exit)";
+            }));
+
+
+        }
+
+        public void startClient(TcpClient thClientSocket, string clineNo)
+        {
+            this.thClientSocket = thClientSocket;
+            this.clNo = clineNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[65536];
+            string dataFromClient = null;
+            string dataFromClientIP = null;
+            string dataFromClientNickname = null;
+            string dataFromClientMsg = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+            NetworkStream networkStream = null;
+
+            while (true)
+            {
+                requestCount = requestCount + 1;
+
+                try
+                {
+                    
+                    networkStream = thClientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, (int)thClientSocket.ReceiveBufferSize);
+                    
+                    dataFromClient = System.Text.Encoding.UTF8.GetString(bytesFrom);
+                    dataFromClientIP = dataFromClient.Substring(0, dataFromClient.IndexOf(";"));
+                    dataFromClientNickname = dataFromClient.Replace(dataFromClientIP + ";", "");
+                    dataFromClientNickname = dataFromClientNickname.Substring(0, dataFromClientNickname.IndexOf(";"));
+                    dataFromClientMsg = dataFromClient.Replace(dataFromClientIP + ";", "");
+                    dataFromClientMsg = dataFromClientMsg.Replace(dataFromClientNickname + ";", "");
+
+                    dataFromClientMsg = dataFromClientMsg.Substring(0, dataFromClientMsg.IndexOf("\0"));
+
+                    /*
+                    this.Invoke(new EventHandler(delegate {
+                        txtLog.Text = txtLog.Text +
+                                  Environment.NewLine +
+                                  ">> 클라이언트로 부터(From Client):" +
+                                  clNo + " " + dataFromClientMsg;
+                    }));
+                    */
+                    this.Invoke(new EventHandler(delegate {
+                        txtLog.Text = txtLog.Text +
+                                  Environment.NewLine +
+                                  ">> :" + clNo + " " + dataFromClientMsg;
+                    }));
+
+
+                    rCount = Convert.ToString(requestCount);
+                    serverResponse = "서버 to 클라이언트(Server to client) (" + clNo + ") " + rCount;
+                    //sendBytes = Encoding.UTF8.GetBytes(serverResponse);
+
+                    sendBytes = Encoding.UTF8.GetBytes(txtLog.Text);
+
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.FlushAsync();
+
+                    /*
+                    this.Invoke(new EventHandler(delegate {
+                        txtLog.Text = txtLog.Text + Environment.NewLine +
+                                  ">>" + serverResponse;
+                    }));
+                    */
+
+                }
+                catch (Exception ex)
+                {
+                    //this.Invoke(new EventHandler(delegate {
+                    //    txtLog.Text = txtLog.Text + Environment.NewLine + ">>" + ex.ToString();
+                    //}));
+
+                    //requestCount = requestCount - 1;
+                }
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("서버를 종료합니다.(Shutdown the server.)", "메시지(Messages)", 
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.ExitThread();
+            Environment.Exit(0);
+        }
+    }
+
+
+}
